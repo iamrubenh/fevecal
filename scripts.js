2a26165a4b27e182748ffe63e42e969cebff553b
Ruben Hernandez <iamrubenh@gmail.com>
2022-07-04 20:45:24 -0600

    Update

--- scripts.js
+++ scripts.js
@@ -32,3 +32,13 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+$(window).on('unload', function() {
+    $(window).scrollTop(0);
+ });
+ 
+ window.onunload = function(){ window.scrollTo(0,0); }
+ 
+ if ('scrollRestoration' in history) {
+   history.scrollRestoration = 'manual';
+ }
